edcaee416ec7e099da8d79f13eb4e957025b6da1
Matheus-Leffa <matheusleffa@live.com>
2025-12-01 20:17:51 -0300

    Implementação Interface Repository

--- obj/Debug/net9.0/APIUsuarios.AssemblyInfo.cs
+++ obj/Debug/net9.0/APIUsuarios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIUsuarios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36c4458c3a54e1a6502d1f3f04b63ec5f68dae25")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIUsuarios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIUsuarios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
