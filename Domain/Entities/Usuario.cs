36c4458c3a54e1a6502d1f3f04b63ec5f68dae25
Matheus-Leffa <matheusleffa@live.com>
2025-12-01 20:10:35 -0300

    Implementação entidade Usuário

---
+++ Domain/Entities/Usuario.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+
+public  class  Usuario
+
+{
+[Key]
+public  int Id { get; set; }
+
+[Required]
+[MinLength(3)]
+[MaxLength(100)]
+public  string Nome { get; set; } = string.Empty;
+
+[Required]
+public  string Email { get; set; }
+
+[Required]
+[MinLength(6)]
+public  string Senha { get; set; }
+
+[Required]
+public  DateTime DataNascimento { get; set; }
+
+public  string Telefone { get; set; }
+
+[Required]
+public  bool Ativo { get; set; } = true;
+
+public  DateTime DataCriacao { get; set; } = DateTime.Now;
+
+public  DateTime? DataAtualizacao { get; set; } = DateTime.Now;
+
+}
